d09ee12374658b60a39b2536c9938a40b5a7c481
Alfiansyah pradinata putra <144076206+Alfiansyahp2@users.noreply.github.com>
2026-07-23 00:24:48 +0700

    feat(api): setup url routing infrastructure for all core modules

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -14,6 +14,8 @@ urlpatterns = [
     path('api/hr/', include('hr_module.urls')),
     path('api/finance/', include('finance_module.urls')),
     path('api/inventory/', include('inventory_module.urls')),
+    path('api/purchasing/', include('purchasing_module.urls')),
+    path('api/sales/', include('sales_module.urls')),
     path('api/rbac/', include('rbac.urls')),
     path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
