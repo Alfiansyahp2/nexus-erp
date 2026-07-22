60ac1b1a0eac120b3e7370cb1d56d5103570ae93
Alfiansyah pradinata putra <144076206+Alfiansyahp2@users.noreply.github.com>
2026-07-23 00:21:58 +0700

    chore(backend): scaffold core business modules

---
+++ backend/purchasing_module/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PurchasingModuleConfig(AppConfig):
+    name = 'purchasing_module'
---
+++ backend/sales_module/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SalesModuleConfig(AppConfig):
+    name = 'sales_module'
